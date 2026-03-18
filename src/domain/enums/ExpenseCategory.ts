8b7f4750d8e176ac01d4bd4ecce22cb2c72e6c95
Jéssica Ellen <jessicaellen.nunes@gmail.com>
2026-03-18 18:16:32 -0300

    refactor: melhora nomes das categorias de despesas

--- src/domain/enums/ExpenseCategory.ts
+++ src/domain/enums/ExpenseCategory.ts
@@ -1,7 +1,10 @@
-export enum ExpenseCategory {
-  FOOD = "FOOD",
-  TRANSPORT = "TRANSPORT",
-  HEALTH = "HEALTH",
-  EDUCATION = "EDUCATION",
-  OTHER = "OTHER"
-}
+export const ExpenseCategory = {
+  FOOD: "Alimentação",
+  TRANSPORT: "Transporte",
+  HEALTH: "Saúde",
+  EDUCATION: "Educação",
+  OTHER: "Outros",
+} as const;
+
+export type ExpenseCategory =
+  (typeof ExpenseCategory)[keyof typeof ExpenseCategory];
